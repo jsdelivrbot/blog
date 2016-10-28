39512425224b896a6d80e66c3c31d1d3c3564ac8
wenbaofu <wenbaofu@huawei.com>
2016-10-28 22:36:41 +0800

    运行成功

---
+++ AngularJS2/angular-quickstart/app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/10/28 0028.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
+
