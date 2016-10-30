f1cf569d4bc3e95d545749c101d4aad1508b4ee7
wenbaofu <wenbaofu@huawei.com>
2016-10-30 20:42:09 +0800

    新实现的reddit

---
+++ AngularJS2/reddit/app.ts
@@ -0,0 +1,14 @@
+///<reference path="node_modules/angular2/src/core/metadata.d.ts"/>
+/**
+ * Created by Administrator on 2016/10/30 0030.
+ */
+import { bootstrap } from 'angular2/platform/browser';
+import { Component } from 'angular2/core';
+
+@Component({
+    selector : 'reddit',
+    
+})
+
+
+
