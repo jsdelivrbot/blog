152c2f8f17277b7afffe10ea293fbe4451cfc322
wenbaofu <wenbaofu@huawei.com>
2016-11-06 19:42:26 +0800

    英雄编辑器前三步完成

--- AngularJS2/angular-quickstart/app/app.module.ts
+++ AngularJS2/angular-quickstart/app/app.module.ts
@@ -1,9 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { FormsModule} from '@angular/forms'
+
 @NgModule({
-    imports:      [ BrowserModule ],
+    imports:      [ BrowserModule,
+    FormsModule
+     ],
     declarations: [ AppComponent ],
     bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+    
+ }
+
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
--- AngularJS2/angular-quickstart/app/main.ts
+++ AngularJS2/angular-quickstart/app/main.ts
@@ -2,7 +2,3 @@
  * Created by Administrator on 2016/10/28 0028.
  */
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
