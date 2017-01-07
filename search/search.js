bc82e6d6694871e4b41f2d5ecf0c830bbe5d31d8
wenbaofu <wenbaofu@huawei.com>
2017-01-07 21:47:03 +0800

    #16 重新命名文件，修改调用其他js的方式

---
+++ search/search.js
@@ -0,0 +1,23 @@
+document.write("<script language=javascript src=''></script>");
+new_element=document.createElement("script");
+new_element.setAttribute("type","text/javascript");
+new_element.setAttribute("src","https://rawgit.com/GerHobbelt/hilitor/master/hilitor-utf8.js");
+document.body.appendChild(new_element);
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results == null ? "": decodeURIComponent(results[1]);
+}
+
+var myHilitor; // global variable
+document.addEventListener("DOMContentLoaded", function(e) {
+    myHilitor = new Hilitor();
+    var querystring = getParameterByName("query");
+    myHilitor.apply(querystring);
+  }, false);
+
+
+
+
