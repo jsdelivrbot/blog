18f8c6b37424ee9a091f9751694848d79acc89b6
wenbaofu <wenbaofu@huawei.com>
2017-01-07 21:16:10 +0800

    Update test.js
    #16 将待实现的3个功能实现，还有一个页面跳转的能力未实施

--- AngularJS2/reddit/test.js
+++ AngularJS2/reddit/test.js
@@ -1,3 +1,16 @@
-var test = window.location.search;
-alert(test);
+document.write("<script language=javascript src='https://rawgit.com/GerHobbelt/hilitor/master/hilitor-utf8.js'></script>");
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results == null ? "": decodeURIComponent(results[1]);
+}
+
+var myHilitor; // global variable
+document.addEventListener("DOMContentLoaded", function(e) {
+    myHilitor = new Hilitor();
+    var querystring = getParameterByName("query");
+    myHilitor.apply(querystring);
+  }, false);
 
