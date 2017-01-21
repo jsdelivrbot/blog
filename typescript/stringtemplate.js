5af8265208ffdd8943465835143c73f1838b91b3
wenbaofu <wenbaofu@huawei.com>
2017-01-21 21:28:38 +0800

    #17 模板字符串

---
+++ typescript/stringtemplate.js
@@ -0,0 +1,4 @@
+var myname = "wenbaofu";
+var age = 30;
+var temp = "my name is " + myname + ",I will be " + (age + 1) + " next year";
+console.log(temp);
---
+++ typescript/stringtemplate.ts
@@ -0,0 +1,5 @@
+let myname : string = "wenbaofu";
+let age : number = 30;
+
+let temp = `my name is ${myname},I will be ${age + 1} next year`; 
+console.log(temp);
