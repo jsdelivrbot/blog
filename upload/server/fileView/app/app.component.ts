820556acb8c7a9e2aa0925d321262a2b7db6e560
wenbaofu <wenbaofu@huawei.com>
2017-02-05 02:20:43 +0800

    实现了延时搜索能力，并实现了搜索下拉框，暂不支持点击跳转功能 https://github.com/wenbaofu/blog/issues/18#issuecomment-277459371

--- upload/server/fileView/app/app.component.ts
+++ upload/server/fileView/app/app.component.ts
@@ -8,17 +8,22 @@ import { fileService } from './file.Service';
 import { OnInit } from '@angular/core';
 import './rxjs-operators';
 import { Observable }       from 'rxjs/Observable';
-
+import { Subject }          from 'rxjs/Subject';
 
 @Component({
     selector: 'my-app',
     template: `
         <div>
-        <input id="kw" #term (keyup)="search(term.value) "/>
+        <input id="kw" #term (keyup)="search(term.value) " (focus)="enter=true" (focusout)="enter=false"/>
         <label *ngFor="let item of types">
         <input type="radio" disabled name="options">
             {{item}}
         </label>
+        <div class="search-input" *ngIf="enter">
+        <div *ngFor="let file of files" class="search-result" >
+            {{file.title}} | ({{file.author}} @ {{file.uploadTime}})
+        </div>
+        </div>
         
         </div>
         <div>
@@ -26,6 +31,30 @@ import { Observable }       from 'rxjs/Observable';
         </div>
     `,
     styles: [`
+    .search-input{
+        border: 1px solid #ccc!important;
+        width:471px;
+        height:auto;
+        margin-left: 9px;
+        position: absolute;
+        background: white;
+        display:block;
+        z-index:9999;
+    }
+    .search-result{
+  border: none;
+  width:465px;
+  height: 20px;
+  padding: 5px;
+  background-color: white;
+  cursor: pointer;
+  margin-left: 9px;
+  font: 12px arial;
+}
+.search-result:hover {
+      background-color: #dcdcdc;
+      left: .1em;
+    }
        #kw {
     width: 480px!important;
     padding-right: 50px!important;
@@ -58,31 +87,44 @@ export class AppComponent implements OnInit{
     types : string[] = ["全部","视频","文档","其他"];
     
     files : fileInfo[] = [];
+    enter : boolean = false;
+    //files : Observable <fileInfo[]>;
     errorMessage : string = undefined;
+    private searchTermStream = new Subject<string>();
 
     constructor(private fileservice : fileService)
     {
+        this.searchTermStream.debounceTime(500).distinctUntilChanged().
+        switchMap(term => this.fileservice.getFiles(undefined,term)).subscribe(
+            files => this.files = files
+        );
+        
     }
 
     ngOnInit() : void
     {
         this.fileservice.getFiles().subscribe(
-            files => this.files = files,
-            error => this.errorMessage = <any>error 
-
-        );
-        // this.fileservice.getFiles().then(
-        //         files => this.files = files,
-        //     error => this.errorMessage = <any>error
-        // );
-    }
-
-    search (term: string) {
-        this.fileservice.getFiles(undefined,term).subscribe(
                 files => this.files = files,
             error => this.errorMessage = <any>error
         );
-  }
+        
+    }
+
+    
+    
+    search(term: string) 
+    { 
+        this.searchTermStream.next(term); 
+    }
+
+
+
+    // search (term: string) {
+    //     this.fileservice.getFiles(undefined,term).subscribe(
+    //             files => this.files = files,
+    //         error => this.errorMessage = <any>error
+    //     );
+    //}
 
  };
  
